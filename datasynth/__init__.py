f35880092aa5da2f565a284b914aa1c62027351e
tobiadefami <tobiadefami@gmail.com>
2023-03-28 14:39:41 +0100

    fix logging error

--- datasynth/__init__.py
+++ datasynth/__init__.py
@@ -1,4 +1,8 @@
 import os
+import logging 
+
+logger = logging.getLogger("langchain")
+logger.setLevel(logging.ERROR)
 
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
 TEMPLATE_DIR = os.path.join(ROOT_DIR, "templates")
